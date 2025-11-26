e9b9e7e47eb90e270cb9a723ed25f8d048cc33af
DanielFernandez19 <danydf1997@gmail.com>
2025-11-26 19:28:10 -0300

    init repo nextjs proyect

---
+++ app/Login/page.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Login</h1>
+      <input
+        className="border border-gray-300 rounded px-4 py-2"
+        type="text"
+        placeholder="Username"
+      />
+    </div>
+  );
+}
