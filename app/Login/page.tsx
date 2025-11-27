6ad2741a3ce6c6faa8fb80227f1f8cdfb1573bd5
DanielFernandez19 <danydf1997@gmail.com>
2025-11-26 23:35:01 -0300

    Primer commit creacion carpeta login con page

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,12 +1,26 @@
 export default function Login() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Login</h1>
-      <input
-        className="border border-gray-300 rounded px-4 py-2"
-        type="text"
-        placeholder="Username"
-      />
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold m-6">Login</h1>
+      <div className="flex flex-col space-y-4">
+        <input
+          className="border border-gray-300 rounded px-4 py-2"
+          type="text"
+          placeholder="user@gmail.com"
+          id="txtEmail"
+          name="email"
+        />
+        <input
+          className="border border-gray-300 rounded px-4 py-2"
+          type="password"
+          placeholder="Password"
+          id="txtPassword"
+          name="password"
+        />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Login
+        </button>
+      </div>
     </div>
   );
 }
