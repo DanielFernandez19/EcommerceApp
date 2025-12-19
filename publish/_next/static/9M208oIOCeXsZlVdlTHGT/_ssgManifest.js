f8ba47f85bb96209d3fa249b5ca0fa788a7c11ee
DanielFernandez19 <danydf1997@gmail.com>
2025-12-18 21:02:23 -0300

    out de windows

---
+++ publish/_next/static/9M208oIOCeXsZlVdlTHGT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fabm\u002Fproducts\u002Fedit\u002F[id]","\u002Fdashboard\u002Fabm\u002Fproducts\u002Fview\u002F[id]","\u002Fdashboard\u002Fabm\u002Fusers\u002Fedit\u002F[id]","\u002Fdashboard\u002Fabm\u002Fusers\u002Fview\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
